cff87f3f28ed1088aece0860fa2e5bfc6e347a36
Tinko Bartels <mail@tinkobartels.de>
2019-08-13 00:22:43 +0200

    Add triangulation model and delaunay triangulation algorithm.

---
+++ include/boost/geometry/extensions/triangulation/triangulation.hpp
@@ -0,0 +1,15 @@
+// Boost.Geometry (aka GGL, Generic Geometry Library)
+
+// Copyright (c) 2019 Tinko Bartels, Berlin, Germany.
+
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_GEOMETRY_EXTENSIONS_TRIANGULATION_HPP
+#define BOOST_GEOMETRY_EXTENSIONS_TRIANGULATION_HPP
+
+#include <boost/geometry/extensions/triangulation/geometries/triangulation.hpp>
+#include <boost/geometry/extensions/triangulation/algorithms/delaunay_triangulation.hpp>
+
+#endif // BOOST_GEOMETRY_EXTENSIONS_TRIANGULATION_HPP
