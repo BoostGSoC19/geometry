d140ddbb7c7f57f67fa5f7a5080651563bec80c7
Tinko Bartels <mail@tinkobartels.de>
2019-08-13 00:35:32 +0200

    Code formatting.

--- extensions/example/triangulation/triangulation_example.cpp
+++ extensions/example/triangulation/triangulation_example.cpp
@@ -26,9 +26,10 @@ int main()
     typedef bg::model::triangulation<point> triangulation;
 
     std::default_random_engine gen(1);
-    std::uniform_real_distribution<> dist(0.0,1.0);
+    std::uniform_real_distribution<> dist(0.0, 1.0);
     std::vector<point> in;
-    for (int i = 0; i < samples ; ++i) {
+    for (int i = 0 ; i < samples ; ++i)
+    {
         in.push_back(point(dist(gen), dist(gen)));
     }
     triangulation out(samples);
@@ -36,14 +37,17 @@ int main()
 
     std::ofstream svg("triangulation.svg");
     bg::svg_mapper<point> mapper(svg, 720, 720);
-    mapper.add(point(0,0));
-    mapper.add(point(1,1));
+    mapper.add(point(0, 0));
+    mapper.add(point(1, 1));
 
-    for(auto const& f : bg::face_range(out)) {
+    for (auto const& f : bg::face_range(out))
+    {
         mapper.map(f, "fill-opacity:0.3;fill:rgb(102,102,201);stroke:rgb(51,51,152);");
     }
+
     typedef bg::model::voronoi_face_view<triangulation> voronoi_face_view;
-    for(auto it = out.vertices_begin(); it != out.vertices_end(); ++it) {
+    for (auto it = out.vertices_begin() ; it != out.vertices_end() ; ++it)
+    {
         voronoi_face_view vfv(out, it);
         mapper.map(voronoi_face_view(out, it), "opacity:1.0;fill:none;stroke:rgb(255,0,0);stroke-width:1");
     }
